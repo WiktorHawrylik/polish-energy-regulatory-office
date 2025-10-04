4191639f4f59e319216bd40599855f8f581581ca
Wiktor <wiktor.hawrylik@lingarogroup.com>
2025-10-04 18:10:42 +0200

    rm html_static_path = ["_static"]

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # Options for HTML output
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
 html_title = f"{project} v{version}"
 
 # Napoleon settings
